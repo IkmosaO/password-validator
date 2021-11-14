06f758d95b72cd1bc1e7d55dadff9434b4b199e3
Ikponmwosa <otasowieik@gmail.com>
2021-11-14 09:34:06 -0500

    added .split method to specChars in specialCharCheck function

--- index.js
+++ index.js
@@ -1,79 +1,61 @@
-// purpose:the function returns true if the password has 8 characters and has 
-// at least one lower case letter, upper case letter, numeric value and special character
-
-// accepted parameter(s): string userpassword
-
-// expected output: boolean
-
 const validatePassword = (userPassword) => {
+  if (userPassword.length < 8) return false
+  let passArray = userPassword.split('')
+  let hasNumb = false
+  let hasUpper = false
+  let hasLower = false
+  let hasSpec = false
 
-    // if password is less than 8 characters return false
-    if (userPassword.length < 8) return false
+  for (let i = 0; i < passArray.length; i++) {
+    const currentChar = passArray[i]
 
-    // split the userPassword into an array
-    let passArray = userPassword.split('')
-
-    let hasNumb = false
-    let hasUpper = false
-    let hasLower = false
-    let hasSpec = false
-
-    // loop through each character
-    for(let i = 0; i < passArray.length; i++) {
-        const currentChar = passArray[i]
-
-        // call in each functions using parameter
-        if (numberCheck(currentChar)) {
-            hasNumb = true
-            continue
-        }
-
-        if (upperCaseCheck(currentChar)) {
-            hasUpper = true
-            continue
-        }
-
-        if (lowerCaseCheck(currentChar)) {
-            hasLower = true
-            continue
-        }
-
-        if (specialCharCheck(currentChar)) {
-            hasSpec = true
-            continue
-        }
+    if (numberCheck(currentChar)) {
+      hasNumb = true
+      continue
     }
-    return hasNumb && hasUpper && hasLower && hasSpec
+    if (upperCaseCheck(currentChar)) {
+      hasUpper = true
+      continue
+    }
+    if (lowerCaseCheck(currentChar)) {
+      hasLower = true
+      continue
+    }
+    if (specialCharCheck(currentChar)) {
+      hasSpec = true
+      continue
+    }
+  }
+
+  return hasNumb && hasUpper && hasLower && hasSpec
 }
+const numberCheck = (parameter) => {
+  const numberConverted = Number(parameter)
 
-// function for checking if a parameter is a number
-function numberCheck(parameter) {
-    const numberConverted = Number(parameter)
-
-    return !isNaN(numberConverted)
+  return !isNaN(numberConverted)
 }
-
-// function for checking if a parameter is an uppercase number
 const upperCaseCheck = (parameter) => {
-    if (numberCheck(parameter)) return false
-    return parameter.toUpperCase() === parameter
-}
+  if (numberCheck(parameter)) return false
 
-// function for checking if a parameter is a lowercase number
+  return parameter.toUpperCase() === parameter
+}
 const lowerCaseCheck = (parameter) => {
-    if (numberCheck(parameter)) return false
-    return parameter.toLowerCase() === parameter
+  if (numberCheck(parameter)) return false
+
+  return parameter.toLowerCase() === parameter
 }
 
-// function for checking if a parameter is a special character
 const specialCharCheck = (parameter) => {
-    const specChars = '"~`!#$%^&*+=-[]\\\';,/{}|\":<>?"'
+  const specChars = '~`!#$%^&*:<>?'
+  let charArray = specChars.split('')
 
-    for(let i = 0; i < specChars.length; i++) {
-        const eachChar = specChars[i]
-        if (eachChar === parameter) return true
-    }
-    return false
+  for (let i = 0; i < charArray.length; i++) {
+    const eachChar = charArray[i]
+
+    if (eachChar === parameter) return
+  }
+
+  return false
 }
 
 module.exports = validatePassword
